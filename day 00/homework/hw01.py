6469596bd1cbba846aca3d9eb25d25138bc4d89f
nikaeliashvili77 <nikushaeliashvili208@gmail.com>
2024-10-13 21:19:48 +0400

    first commit

---
+++ day 00/homework/hw01.py
@@ -0,0 +1,49 @@
+from turtle import *
+
+#we wand to paint a house
+
+#stemp 1: draw a square
+speed(30)
+width(7)
+color("purple")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of square
+
+#drawing a dor
+
+forward(70)
+color("yellow")
+left(90)
+forward(120)   #heigh of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+
+penup()
+goto(200,200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+#end of drawing a door
+
+
+
+
+exitonclick()
