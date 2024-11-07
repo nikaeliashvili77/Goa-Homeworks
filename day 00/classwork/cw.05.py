c90c9c338fc1baf1c32d84d609d82acfdeaae87e
nikaeliashvili77 <nikushaeliashvili208@gmail.com>
2024-11-07 16:21:41 +0400

    Create cw.05.py

---
+++ day 00/classwork/cw.05.py
@@ -0,0 +1,46 @@
+# 1
+greeting = "Hello"
+username = "student"
+age = '20'
+location = "New York"
+
+#2
+print('Welcome, ' + username + "!" ) 
+print(greeting + ", " + username + "!") 
+
+#3
+print("Your age is " + age)  
+print("In 5 years, you will be " + str(int(age) + 5) + " years old") 
+
+#4
+favoriteNumber = 7
+doubled_number = favoriteNumber * 2
+print("Your favorite number doubled is " + str(doubled_number))
+
+#5
+total_score = 100
+bonus_points = 10
+finalScore = total_score + bonus_points
+print("Your final score is " + str(finalScore))
+
+#6
+days_in_a_week = 7
+print("There are " + str(days_in_a_week * 4) + " days in four weeks")
+
+#7
+firstName = "John"
+lastName = "Smith"
+print("Hello, " + firstName + " " + lastName + "!")
+
+#8
+nickname = "Ace"
+print("Your nickname is: " + nickname)
+
+#10
+year = 2024
+print("Next year is " + str(year + 1))
+print("Your age next year will be " + str(int(age) + 1))
+
+#11
+farewell = "Goodbye"
+print(farewell, username + "! See you next time.")
